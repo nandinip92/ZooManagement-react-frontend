840b6b68dabf6bcd680775afa7f58a1f4d6b217a
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-04-29 06:26:55 +0100

    feat: creating species component

--- src/components/species/species.tsx
+++ src/components/species/species.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Species:React.FC=()=>{
+
+    const [species,setSpecies]= useState<string>();
     return <></>
 }
