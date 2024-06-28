010ef7de192aad2570dac05ce33126eec9944b1b
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-06-28 16:13:28 +0100

    feat: Using path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": `${path.resolve(__dirname, "./src/components")}`,
+      "@api": `${path.resolve(__dirname, "./src/api")}`,
+      type: `${path.resolve(__dirname, "./src/types")}`,
+      models: `${path.resolve(__dirname, "./src/models")}`,
+      requests: `${path.resolve(__dirname, "./src/models/request")}`,
+      views: `${path.resolve(__dirname, "./src/models/view")}`,
+      public: `${path.resolve(__dirname, "./public")}`,
+      pages: path.resolve(__dirname, "./src/pages"),
+    },
+  },
 })
