3dec90d0efbfd13d89aa24fadbd2c9ab32964a9f
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-07-04 16:55:48 +0100

    chore: Adding assets custom path

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@components": `${path.resolve(__dirname, "./src/components")}`,
+      "@assets": path.resolve(__dirname, "src/assets"),
       "@api": `${path.resolve(__dirname, "./src/api")}`,
       type: `${path.resolve(__dirname, "./src/types")}`,
       models: `${path.resolve(__dirname, "./src/models")}`,
@@ -16,6 +17,7 @@ export default defineConfig({
       views: `${path.resolve(__dirname, "./src/models/view")}`,
       public: `${path.resolve(__dirname, "./public")}`,
       pages: path.resolve(__dirname, "./src/pages"),
+      speciesImages: path.resolve(__dirname, "./src/assets/Images"),
     },
   },
 })
